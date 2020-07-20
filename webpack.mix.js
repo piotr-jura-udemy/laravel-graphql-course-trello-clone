3ccf119bad3ef845b8b414ef7a2b1b3f84d47b99
Piotr Jura <>
2020-07-20 10:21:27 +0200

    #034 Initializing Vue and Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss')
+]);
+
+mix.js('resources/js/app.js', 'public/js');
