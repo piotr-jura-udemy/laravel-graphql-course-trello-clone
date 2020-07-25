40517661616d0405f24f0ff968df708212ba29a8
Piotr Jura <>
2020-07-25 16:20:26 +0200

    #044 Mutations inside Vue component using this.$apollo.mutate({...})

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -1,5 +1,25 @@
 <template>
   <div
+    @click="addCard"
     class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm"
   >Add new card</div>
 </template>
+
+<script>
+import CardAdd from "./../graphql/CardAdd.gql";
+
+export default {
+  methods: {
+    addCard() {
+      this.$apollo.mutate({
+        mutation: CardAdd,
+        variables: {
+          title: "Added through mutation",
+          listId: 1,
+          order: 1
+        }
+      });
+    }
+  }
+};
+</script>
