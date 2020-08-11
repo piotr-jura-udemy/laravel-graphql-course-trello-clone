f01b946dc3060db4df8e4d9ace714ffba645f29a
Piotr Jura <>
2020-08-11 20:29:36 +0200

    #091 Vue Clickaway Directive + Closing on Route Change

--- resources/js/components/DropdownMenu.vue
+++ resources/js/components/DropdownMenu.vue
@@ -1,6 +1,7 @@
 <template>
   <transition name="appear">
     <div
+      v-on-clickaway="close"
       v-if="show"
       class="dropdown-menu absolute bg-gray-200 rounded-sm mt-2 text-sm text-gray-600 border-gray-200 shadow w-64 overflow-y-auto z-10 p-2"
     >
@@ -10,9 +11,16 @@
 </template>
 
 <script>
+import { directive as onClickaway } from "vue-clickaway";
 export default {
   props: {
     show: Boolean
+  },
+  directives: { onClickaway },
+  methods: {
+    close() {
+      this.$emit("closed");
+    }
   }
 };
 </script>
--- resources/js/components/UserBoardsDropdown.vue
+++ resources/js/components/UserBoardsDropdown.vue
@@ -2,7 +2,7 @@
   <div>
     <button class="header-btn" @click.prevent="showBoards = !showBoards">Boards</button>
 
-    <DropdownMenu :show="showBoards">
+    <DropdownMenu :show="showBoards" @closed="showBoards = false">
       <div class="text-gray-600 text-xs font-semibold mb-2 ml-2">BOARDS</div>
 
       <router-link
@@ -11,6 +11,7 @@
         :key="board.id"
         :class="[colorMap100[board.color]]"
         class="m-2 rounded-sm opacity-100 hover:opacity-75 text-gray-700 font-bold cursor-pointer flex"
+        @click.native="showBoards = false"
       >
         <div class="w-10 rounded-sm rounded-r-none" :class="[colorMap200[board.color]]"></div>
         <div class="p-2">{{ board.title }}</div>
